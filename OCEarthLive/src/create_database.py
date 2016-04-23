b00fc8c10291b57b3f44d1fa3f4ee9054781b480
Neil Godfrey <godfrey4000@gmail.com>
2016-04-23 07:28:27 +0000

    First commit

---
+++ OCEarthLive/src/create_database.py
@@ -0,0 +1,57 @@
+'''
+Created on Apr 22, 2016
+
+@author: neil
+'''
+import sqlite3
+
+con = sqlite3.connect('/tmp/tweets.db')
+
+with con:
+    cursor = con.cursor()
+    
+    # EONet -- the EONet events.
+    cursor.execute('''
+        DROP TABLE IF EXISTS EONet
+    ''')
+    cursor.execute('''
+        CREATE TABLE EONet(
+            eonet_id TEXT PRIMARY KEY,
+            title TEXT,
+            hashtag TEXT,
+            geometries BLOB,
+            json_geometries TEXT
+        )''')
+    
+    # Tweet - parsed data from the tweet.
+    cursor.execute('''
+        DROP TABLE IF EXISTS Tweet
+    ''')
+    cursor.execute('''
+        CREATE TABLE Tweet(
+            tweet_id TEXT PRIMARY KEY,
+            eonet_id INTEGER,
+            name TEXT,
+            createDate TEXT,
+            place TEXT,
+            msg TEXT,
+            coordinates BLOB,
+            json_coordinates TEXT,
+            FOREIGN KEY(eonet_id) REFERENCES EONet(eonet_id)
+        )''')
+    
+    # Photo -- photos attached to the tweets.
+    cursor.execute('''
+        DROP TABLE IF EXISTS Photo
+    ''')
+    cursor.execute('''
+        CREATE TABLE Photo(
+            photo_id INTEGER PRIMARY KEY,
+            tweet_id TEXT,
+            media_url TEXT,
+            FOREIGN KEY(tweet_id) REFERENCES Tweet(tweet_id)
+        )''')
+    
+    # Commit saves the changes.
+    con.commit()
+            
---
+++ OCEarthLive/src/osgeo_test.py
@@ -0,0 +1,58 @@
+'''
+Created on Apr 21, 2016
+
+@author: neil
+'''
+from osgeo import ogr
+
+
+x = (-95.980224609375 - 94.64599609375)/2
+y = (29.213727993972313 + 30.36072451862922)/2
+print ('%f %f' % (x, y))
+
+geojson_poly = """{
+                    "date": "2016-04-17T00:00:00Z",
+                    "type": "Polygon", 
+                    "coordinates": [[ [-95.980224609375, 29.213727993972313], [-95.980224609375, 30.36072451862922], [-94.64599609375, 30.36072451862922], [-94.64599609375, 29.213727993972313], [-95.980224609375, 29.213727993972313] ]]
+                }"""
+                
+yabba = """{"date": "2016-04-13T00:00:00Z", "type": "Polygon", "coordinates": [[44.4140625, 30.877888118431578], [44.4140625, 38.9142723125973], [49.912109375, 38.9142723125973], [49.912109375, 30.877888118431578], [44.4140625, 30.877888118431578]]}"""
+dabba = """{
+                    "date": "2016-04-13T00:00:00Z",
+                    "type": "Polygon", "coordinates": [[ [44.4140625, 30.877888118431578], [44.4140625, 38.9142723125973], [49.912109375, 38.9142723125973], [49.912109375, 30.877888118431578], [44.4140625, 30.877888118431578] ]]
+                }"""
+anotherjson = """{
+                    u'date': u'2016-04-20T00:00:00Z',
+                    u'type': u'Point',
+                     u'coordinates': [-178.2, -13.1]},
+                {
+                    u'date': u'2016-04-20T06:00:00Z',
+                    u'type': u'Point',
+                    u'coordinates': [-178.8, -12.9]},
+                {
+                    u'date': u'2016-04-20T12:00:00Z',
+                    u'type': u'Point',
+                    u'coordinates': [-178.8, -12.8]},
+                {
+                    u'date': u'2016-04-20T18:00:00Z',
+                    u'type': u'Point',
+                    u'coordinates': [-179.6, -12.6]},
+                {
+                    u'date': u'2016-04-21T00:00:00Z',
+                    u'type': u'Point',
+                    u'coordinates': [-179.5, -12.5]},
+                {
+                    u'date': u'2016-04-21T06:00:00Z',
+                    u'type': u'Point',
+                    u'coordinates': [-179.3, -13.0]}, {u'date': u'2016-04-21T12:00:00Z', u'type': u'Point', u'coordinates': [-178.9, -12.7]}, {u'date': u'2016-04-21T18:00:00Z', u'type': u'Point', u'coordinates': [-178.3, -12.7]}, {u'date': u'2016-04-22T00:00:00Z', u'type': u'Point', u'coordinates': [-177.4, -12.4]}, {u'date': u'2016-04-22T06:00:00Z', u'type': u'Point', u'coordinates': [-177.0, -12.5]}"""
+#geojson_point = """{"type":"Point","coordinates":[108420.33,753808.59]}"""
+geojson_point = """{"type":"Point","coordinates":[-95.313,29.787]}"""
+point = ogr.CreateGeometryFromJson(geojson_point)
+poly = ogr.CreateGeometryFromJson(yabba)
+poanothely = ogr.CreateGeometryFromJson(anotherjson)
+omg = ogr.CreateGeometryFromJson(geojson_poly)
+print "%d,%d" % (point.GetX(), point.GetY())
+#if point.Within(poly):
+#    print 'Yep.'
+#else:
+#    print 'Nope.'
